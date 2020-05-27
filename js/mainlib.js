8ba7a902a97aa671463d865079a910cc473dee05
VividerAphid <trever.kress@gmail.com>
2020-05-26 22:32:10 -0400

    Route graphics added

--- js/mainlib.js
+++ js/mainlib.js
@@ -1,13 +1,18 @@
 function render(map, g){
     console.log("render called");
     for(var r = 0; r < map.length; r++){
+        map[r].drawRoutes(g, map);
+    }
+    for(var r = 0; r < map.length; r++){
         map[r].drawStar(g);
     }
 }
 
 function loadMap(){
     console.log("load map");
-    var map = [new star(1, 20, 20, "mins", 100, 1, "MinsClan", "#0ff", "2;3", 1, 42)];
+    var map = [new star(0, 50, 50, "mins1", 100, 1, "MinsClan", "#0ff", "1;2", 1, 42),
+        new star(1, 150, 150, "mins2", 100, 1, "MinsClan", "#0ff", "0;2", 1, 55),
+        new star(2, 150, 50, "mins3", 100, 2, "MinsClan2", "#f0f", "0;1", 2, 75)];
     return map;
 }
 
--- js/star.js
+++ js/star.js
@@ -7,7 +7,7 @@ function star(id, x, y, name, value, clanId, clanName, colour, routes, factionId
     this.clanId = clanId;
     this.clanName = clanName;
     this.colour = colour;
-    this.routes = routes;
+    this.routes = this.convertRoutes(routes);
     this.factionId = factionId;
     this.production = production;
 
@@ -32,4 +32,35 @@ star.prototype.drawStar = function(G){
         G.arc(this.x, this.y, this.radius*2 + 8, 0, 2*Math.PI);
         G.stroke();
     }
+}
+
+star.prototype.drawRoutes = function(G, map){
+    var reps = this.routes.length;
+    for(var r = 0; r < reps; r++){
+        var connectee = map[this.routes[r]].id;
+        if(connectee > this.id){
+            G.beginPath();
+            G.lineWidth = 3;
+            if(this.factionId == map[connectee].factionId){
+                G.fillStyle = this.colour;
+                G.strokeStyle = this.colour;
+            }
+            else{
+                G.fillStyle = "#999";
+                G.strokeStyle = "#999";
+            }
+            G.moveTo(this.x, this.y);
+            G.lineTo(map[connectee].x, map[connectee].y);
+            G.stroke();
+        }
+    }
+}
+
+star.prototype.convertRoutes = function(rout){
+    var routeStr = rout.split(";");
+    console.log(routeStr);
+    for(var r = 0; r < routeStr.length; r++){
+        routeStr[r] = routeStr[r] * 1;
+    }
+    return routeStr;
 }
