c880bdbe40b03d32d5b1ddd1b5c76e9386a1f206
VividerAphid <trever.kress@gmail.com>
2020-05-26 20:17:23 -0400

    More file setup

---
+++ js/star.js
@@ -0,0 +1,13 @@
+function star(id, x, y, name, value, clanId, clanName, colour, routes, factionId, production){
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.name = name;
+    this.value = value;
+    this.clanId = clanId;
+    this.clanName = clanName;
+    this.colour = colour;
+    this.routes = routes;
+    this.factionId = factionId;
+    this.production = production;
+}
