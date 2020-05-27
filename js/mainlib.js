0a4a0d8293ae32d3c41a6c7f9132a986a4203491
VividerAphid <trever.kress@gmail.com>
2020-05-26 20:50:13 -0400

    Star rendering added

--- js/mainlib.js
+++ js/mainlib.js
@@ -1,9 +1,14 @@
-function render(){
+function render(map, g){
     console.log("render called");
+    for(var r = 0; r < map.length; r++){
+        map[r].drawStar(g);
+    }
 }
 
 function loadMap(){
     console.log("load map");
+    var map = [new star(1, 20, 20, "mins", 100, 1, "MinsClan", "#0ff", "2;3", 1, 42)];
+    return map;
 }
 
 function readFiles(){
--- js/star.js
+++ js/star.js
@@ -10,41 +10,22 @@ function star(id, x, y, name, value, clanId, clanName, colour, routes, factionId
     this.routes = routes;
     this.factionId = factionId;
     this.production = production;
+
+    this.radius = 10;
     this.selected = false;
 }
 
-star.prototype.drawStar = function(){
+star.prototype.drawStar = function(G){
     G.beginPath();
-    if(settings.fog){
-        G.fillStyle = "#666";
-        G.strokeStyle = "#666";
-    }
-    else{
-        //console.log("hit?");
-        G.fillStyle = this.teamColour;
-        G.strokeStyle = this.teamColour;
-        //console.log(G.fillStyle + " / " + G.strokeStyle);
-    }
+
+    G.fillStyle = this.colour;
+    G.strokeStyle = this.colour;
+
     G.arc(this.x, this.y, (this.radius*2), 0, 2*Math.PI);
     G.fill();
     G.stroke();
-    
-    if(settings.fog == false){
-        G.beginPath();
-        if(this.lockLife > 0){
-            G.fillStyle = "#000";
-            G.strokeStyle = "#000";
-        }
-        else{
-            G.fillStyle = this.colour;
-            G.strokeStyle = this.colour;
-        }
-        G.arc(this.x, this.y, (this.radius*2) - 5, 0, 2*Math.PI);
-        G.fill();
-        G.stroke();
-    }
 
-    if(this.hasShield && settings.fog == false){
+    if(this.selected){
         G.lineWidth = 2;
         G.strokeStyle = "#0ff";
         G.beginPath();
