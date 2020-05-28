52a31423fb83dbe66457f33cc0f2b3c76ab72357
VividerAphid <trever.kress@gmail.com>
2020-05-27 22:16:06 -0400

    Modularised paths for easier edits

--- retriever.py
+++ retriever.py
@@ -3,10 +3,10 @@ import urllib.request
 print('Retrieving!')
 
 url = 'http://www.galcon.com/g2/logs/'
+files = ['GALAXY.txt', 'HITS.txt']
+path = '/media/PISERVERSTO/galaxyRawFiles/'
+gmFolder = 'GM-5-26-2020/'
+stamp = '1'
 
-targs = ['GALAXY.txt', 'HITS.txt']
-
-stamp = "1"
-
-urllib.request.urlretrieve(url + targs[0], '/media/PISERVERSTO/galaxyRawFiles/GM-5-26-2020/' + stamp + targs[0])
-urllib.request.urlretrieve(url + targs[1], '/media/PISERVERSTO/galaxyRawFiles/GM-5-26-2020/' + stamp + targs[1])
+urllib.request.urlretrieve(url + files[0], path + gmFolder + stamp + files[0])
+urllib.request.urlretrieve(url + files[1], path + gmFolder + stamp + files[1])
