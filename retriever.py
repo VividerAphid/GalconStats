d73a8f4df1f2c679f517e7342eab15e42a26c5ce
VividerAphid <trever.kress@gmail.com>
2020-05-27 22:03:45 -0400

    Updated retriever to retrieve multiple files? and store them in appropriate folder

--- retriever.py
+++ retriever.py
@@ -1,9 +1,12 @@
 import urllib.request
 
-print('Retrieving?')
+print('Retrieving!')
 
-url='http://www.galcon.com/g2/logs/GALAXY.txt'
+url = 'http://www.galcon.com/g2/logs'
 
-yes = "asdf"
+targs = ['/GALAXY.txt', '/HITS.txt']
 
-urllib.request.urlretrieve(url, '/home/pi/Desktop/' + yes + 'galaxy.txt')
+stamp = "1"
+
+urllib.request.urlretrieve(url + targs[0], '/media/PISERVERSTO/galaxyRawFiles/GM-5-26-2020' + stamp + targs[0])
+urllib.request.urlretrieve(url + targs[1], '/media/PISERVERSTO/galaxyRawFiles/GM-5-26-2020' + stamp + targs[1])
