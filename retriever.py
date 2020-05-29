09f076f71925624e10d2826e48874905544d3fbe
VividerAphid <trever.kress@gmail.com>
2020-05-29 16:40:37 -0400

    Converted file retrieving to function, added prep_directory function

--- retriever.py
+++ retriever.py
@@ -1,12 +1,25 @@
 import urllib.request
+import os
 
-print('Retrieving!')
 
-url = 'http://www.galcon.com/g2/logs/'
-files = ['GALAXY.txt', 'HITS.txt']
-path = '/media/PISERVERSTO/galaxyRawFiles/'
-gmFolder = 'GM-5-26-2020/'
-stamp = '1'
+def retrieve_files():
+    print('Retrieving!')
 
-urllib.request.urlretrieve(url + files[0], path + gmFolder + stamp + files[0])
-urllib.request.urlretrieve(url + files[1], path + gmFolder + stamp + files[1])
+    url = 'http://www.galcon.com/g2/logs/'
+    files = ['GALAXY.txt', 'HITS.txt']
+    path = '/media/PISERVERSTO/galaxyRawFiles/'
+    gmFolder = 'GM-5-26-2020/'
+    stamp = '1'
+
+    urllib.request.urlretrieve(url + files[0], path + gmFolder + stamp + files[0])
+    urllib.request.urlretrieve(url + files[1], path + gmFolder + stamp + files[1])
+
+def prep_directory(parent, newDir):
+    print('preparing new folder...')
+    path = os.path.join(parent, newDir)
+    try:  
+        os.mkdir(path)  
+    except OSError as error:  
+        print(error)
+
+prep_directory('/media/PISERVERSTO/galaxyRawFiles/', 'tst')
